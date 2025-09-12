f47b4aa39e9ea35246436e48dae315b3c516ea3f
nguyennam7324 <minhhoangk025@gmail.com>
2025-09-13 04:43:29 +0700

    cm

---
+++ Operator/Operator.js
@@ -0,0 +1,12 @@
+console.log("toan tu")
+//cac toan tu hay xai
+// +,-,*,/,%,
+let a = 5, c = 11
+const b = 10
+console.log(a % b)
+console.log(a)
+//== so sanh gia tri khong bao gom kieu du lieu
+const d = 10
+const f = '10'
+///=== so sanh gia tri co bao gom kieu du lieu
+console.log(d === f)
