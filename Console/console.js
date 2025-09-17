87637a34524348282787ee8971e8abd13f693259
nguyennam7324 <minhhoangk025@gmail.com>
2025-09-18 01:51:46 +0700

    he he

---
+++ Console/console.js
@@ -0,0 +1,11 @@
+console.log("hii")
+
+//co 3 loai console chinh
+// console.log("Test Infor")
+// console.warn("Test Warning")
+// console.error("Test Error")
+
+
+const name = "namalone"
+let age = 20
+console.log(`toi ten la:${name},tuoi la:${age}`)
---
+++ Lab1/Lab1.js
@@ -0,0 +1,17 @@
+console.log("chay dc chua")
+//dm deo nho
+//tinh nam hien tai
+
+const fullName = "Nguyễn Đức Nam"
+const birthYear = 2005
+let isStudent = true
+const today = new Date();
+const currentYear = today.getFullYear()
+
+const caculatedAge = currentYear - birthYear
+
+
+console.log(`Tên: ${fullName}\nTuổi: ${caculatedAge}\nSinhviên: ${isStudent}`)
+
+
+
